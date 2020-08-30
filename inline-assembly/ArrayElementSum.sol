a03391d50ac7376fe880c0ebd6822c5bc67ecf6a
decentralMind <decentralmind@gmail.com>
2020-08-30 23:32:30 +0545

    comment typo fixed for ArrayElementSum.sol

--- inline-assembly/ArrayElementSum.sol
+++ inline-assembly/ArrayElementSum.sol
@@ -3,10 +3,10 @@ pragma solidity >= 0.5 .0 < 0.7 .0;
 contract arrayElementSum{
 
   /*
-   * @dev Take note on memory loading. For the arugment [3,4], the free memory pointer
+   * @dev Take note on memory management and free mmemory pointer. For the arguments [3,4], the free memory pointer
    * which is 0x80 actually contains length of the _data array, to access array index 0,  which is 3,
    * free memory pointer 0x80 needs to be increase by 32 bytes(0x20) and to access array
-   * element 1 which is 4, free memory pointer 0x80 needs to icnrease by 64 bytes(0x40).
+   * element 1 which is 4, free memory pointer 0x80 needs to increase by 64 bytes(0x40).
    **/
 
   function arraySum(uint[] memory _data) public pure returns(uint sum) {
@@ -23,7 +23,7 @@ contract arrayElementSum{
 
 
       // Get the current memory pointer
-      // Increase the length of data with 1.
+      // Increase the length of data with 32 bytes.
       let data: = add(_data, 0x20)
 
       // Iterate until the bound is not met.
